e51da0f3384d0e66f5f427aae37015cc49c8573f
Er-SourabhSingh <er.sourabh.singh0601@gmail.com>
2026-06-25 17:46:34 +0530

    feat: add dark mode toggle

--- sum.js
+++ sum.js
@@ -2,4 +2,9 @@ function sum(a, b) {
   return a + b;
 }
 
-module.exports = sum;
+// dark mode preference helper
+function isDarkMode() {
+  return typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+module.exports = { sum, isDarkMode };
